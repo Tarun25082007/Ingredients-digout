8b900a492aede5a8ba6b4d4bf5c5721377b2425c
Tarun <gopi.tarun1978@gmail.com>
2026-08-10 21:12:04 +0530

    Step 11: Add missing Spring Boot main application class

---
+++ src/main/java/com/digout/DigoutApplication.java
@@ -0,0 +1,12 @@
+package com.digout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigoutApplication.class, args);
+    }
+}
